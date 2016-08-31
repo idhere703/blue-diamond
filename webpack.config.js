1e9579640f46b6c49105ea881c3dff52f46ad642
Brandon <idhere703@gmail.com>
2016-08-30 22:37:22 -0400

    Added bable.

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,18 @@ let BUILD_DIR = path.resolve(__dirname, './deploy');
 let APP_DIR = path.resolve(__dirname, './app');
 
 let config = {
-  entry: APP_DIR + '/app.js',
-  output: {
-    path: BUILD_DIR,
-    filename: '/bundle.js'
-  }
+    entry: APP_DIR + '/app.jsx',
+    output: {
+        path: BUILD_DIR,
+        filename: '/bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?/,
+            include: APP_DIR,
+            loader: 'babel'
+        }]
+    }
 };
 
 module.exports = config;
