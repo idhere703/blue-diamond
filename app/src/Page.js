8f075650dee61200d766c3f923d9e75afbfafdbf
Brandon <idhere703@gmail.com>
2016-12-10 23:11:03 -0500

    Added content and state for organization.

--- app/src/Page.js
+++ app/src/Page.js
@@ -2,11 +2,12 @@ import React from 'react';
 class Page extends React.Component {
   constructor(props) {
     super(props);
+
   }
 
   render() {
     <div>
-
+      Placeholder content.
     </div>
   }
 }
--- app/src/org.js
+++ app/src/org.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Org extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      orgInfo: {
+        
+      }
+    };
+  }
+
   render() {
     return (
       <li id={this.props.orgId}>{this.props.orgName}-{this.props.orgId}</li>
